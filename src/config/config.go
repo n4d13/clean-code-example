ac541d534679816e5dd5c66e4d352adfb44af881
gacevedo <acevedo.gaston@gmail.com>
2018-08-07 16:35:45 -0300

    Refactor of Configuration Manager, Typed properties FTW. A direct impact: Simplified MakeRedisConnection() method

--- src/config/config.go
+++ src/config/config.go
@@ -3,32 +3,68 @@ package config
 import (
 	"os"
 	"sync"
+	"strings"
+	"strconv"
+	"log"
+	"time"
 )
 
 type ConfigurationManager struct {
 	Host         string
-	Port         string
-	DialTimeout  string
-	ReadTimeout  string
-	WriteTimeout string
-	PoolSize     string
-	PoolTimeout  string
+	Port         int
+	DialTimeout  time.Duration
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+	PoolSize     int
+	PoolTimeout  time.Duration
 }
 
 var instance *ConfigurationManager
 var once sync.Once
 
 func GetInstance() *ConfigurationManager {
-	once.Do(func(){
+	once.Do(func() {
 		instance = &ConfigurationManager{
-			Host:         os.Getenv("REDIS_HOST"),
-			Port:         os.Getenv("REDIS_PORT"),
-			DialTimeout:  os.Getenv("REDIS_DIAL_TIMEOUT"),
-			ReadTimeout:  os.Getenv("REDIS_READ_TIMEOUT"),
-			WriteTimeout: os.Getenv("REDIS_WRITE_TIMEOUT"),
-			PoolSize:     os.Getenv("REDIS_POOL_SIZE"),
-			PoolTimeout:  os.Getenv("REDIS_POOL_TIMEOUT"),
+			Host:         getValueOrDefault("REDIS_HOST", ""),
+			Port:         toInt(getValueOrFail("REDIS_PORT")),
+			DialTimeout:  toDuration(getValueOrDefault("REDIS_DIAL_TIMEOUT", "2")),
+			ReadTimeout:  toDuration(getValueOrDefault("REDIS_READ_TIMEOUT", "2")),
+			WriteTimeout: toDuration(getValueOrDefault("REDIS_WRITE_TIMEOUT", "1")),
+			PoolSize:     toInt(getValueOrDefault("REDIS_POOL_SIZE", "5")),
+			PoolTimeout:  toDuration(getValueOrDefault("REDIS_POOL_TIMEOUT", "30")),
 		}
 	})
 	return instance
+}
+
+func (c *ConfigurationManager) Address() string {
+	return c.Host + ":" + strconv.Itoa(c.Port)
+}
+
+func getValueOrFail(name string) string {
+	val := os.Getenv(name)
+	if strings.TrimSpace(val) == "" {
+		log.Fatal("Required value is missing: " + name)
+	}
+	return val
+}
+
+func getValueOrDefault(name string, defaultValue string) string {
+	val := os.Getenv(name)
+	if strings.TrimSpace(val) == "" {
+		return defaultValue
+	}
+	return val
+}
+
+func toInt(val string) int {
+	result, err := strconv.Atoi(val)
+	if err != nil {
+		log.Fatal("Error converting to int. Value: " + val)
+	}
+	return result
+}
+
+func toDuration(val string) time.Duration {
+	return time.Duration(toInt(val)) * time.Second
 }
--- src/db/db.go
+++ src/db/db.go
@@ -2,9 +2,7 @@ package db
 
 import (
 	"github.com/go-redis/redis"
-	"time"
 	"github.com/n4d13/clean-code-example/src/config"
-	"strconv"
 )
 
 type RedisConnection struct {
@@ -13,44 +11,17 @@ type RedisConnection struct {
 
 func MakeRedisConnection() (*RedisConnection, error) {
 
-	address := config.GetInstance().Host+":"+config.GetInstance().Port
-
-	dialTimeout, err := strconv.Atoi(config.GetInstance().DialTimeout)
-	if err != nil{
-		return nil, err
-	}
-
-	readTimeout, err := strconv.Atoi(config.GetInstance().ReadTimeout)
-	if err != nil{
-		return nil, err
-	}
-
-	writeTimeout, err := strconv.Atoi(config.GetInstance().WriteTimeout)
-	if err != nil{
-		return nil, err
-	}
-
-	poolSize, err := strconv.Atoi(config.GetInstance().PoolSize)
-	if err != nil{
-		return nil, err
-	}
-
-	poolTimeout, err := strconv.Atoi(config.GetInstance().PoolTimeout)
-	if err != nil{
-		return nil, err
-	}
-
 	client := redis.NewClient(&redis.Options{
-		Addr:         address,
-		DialTimeout:  time.Duration(dialTimeout) * time.Second,
-		ReadTimeout:  time.Duration(readTimeout) * time.Second,
-		WriteTimeout: time.Duration(writeTimeout) * time.Second,
-		PoolSize:     poolSize,
-		PoolTimeout:  time.Duration(poolTimeout) * time.Second,
+		Addr:         config.GetInstance().Address(),
+		DialTimeout:  config.GetInstance().DialTimeout,
+		ReadTimeout:  config.GetInstance().ReadTimeout,
+		WriteTimeout: config.GetInstance().WriteTimeout,
+		PoolSize:     config.GetInstance().PoolSize,
+		PoolTimeout:  config.GetInstance().PoolTimeout,
 	})
 	_, connErr := client.Ping().Result()
 
-	if connErr != nil{
+	if connErr != nil {
 		return nil, connErr
 	}
 
@@ -59,6 +30,6 @@ func MakeRedisConnection() (*RedisConnection, error) {
 	}, nil
 }
 
-func (r *RedisConnection) Count(keys ...string) int64{
-	 return r.client.Exists(keys...).Val()
+func (r *RedisConnection) Count(keys ...string) int64 {
+	return r.client.Exists(keys...).Val()
 }
